16533b7c1835f3a372fefa6516e949b44db84995
Emily Cowan <e.anderson8977@gmail.com>
2020-04-01 00:37:23 -0700

    Implemented base calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,5 +1,26 @@
-const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
+// const testinput = {
+//   num1: 3,
+//   num2: 0,
+//   operation: '/',
+// }
+
+function calculator(input) {
+  switch(input.operation) {
+    case 'add':
+    case '+':
+      return input.num1 + input.num2;
+    case 'subtract':
+    case '-':
+      return input.num1 - input.num2;
+    case 'multiply':
+    case '*':
+      return input.num1 * input.num2;
+    case 'divide':
+    case '/':
+      return input.num1 / input.num2;
+    default:
+      return 'That is not a valid operation.'
+  }
 }
+
+// console.log(calculator(input))
